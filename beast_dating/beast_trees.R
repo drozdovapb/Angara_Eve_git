4349970d2611fa939cbb6dd0c9c45f07f1b47fbd
drozdovapb <drozdovapb@gmail.com>
2023-05-04 12:56:04 +0800

    upd MRCA tree

--- beast_dating/beast_trees.R
+++ beast_dating/beast_trees.R
@@ -16,24 +16,25 @@ groupcolors <- sapply(beast_tree@phylo$tip.label, function(x) unlist(strsplit(x,
 
 ggtree(beast_tree) + geom_text(aes(label=node), col = "red") + geom_tiplab() + expand_limits(x=15)
 
-tree1 <- 
-ggtree(beast_tree) + theme_tree2()
+tree1 <- ggtree(beast_tree) + theme_tree2()
 tree1 <- revts(tree1)
 
+#svg("mrca_tree.svg", width=5, height=6)
+#png("mrca_tree.png", width=6, height=6, units="in", res=300)
 tree1 + 
   geom_hilight(node=19, fill="#4477AA") + ## southern
   geom_hilight(node=18, fill="#F0E442") + ## western
   geom_hilight(node=20, fill="#D81B60") + ## eastern
   geom_hilight(node=15, fill="#228B22")+ 
-  geom_range("length_0.95_HPD", color="plum2", size=2, alpha=.5) + 
+  geom_range("length_0.95_HPD", color="#8b8ef8", size=2, alpha=.5) + 
 #  geom_text2(aes(label=round(as.numeric(posterior), 2), 
 #                 subset=as.numeric(posterior)> 0.7, 
 #                 x=branch), vjust=0, hjust = 0, color = "grey") + 
   scale_x_continuous(labels = abs, breaks = -10:0) + 
   scale_color_manual(values = c("NA", "black"), guide = 'none')  + 
   geom_text(aes(label = round(height, 1), color = MRCAtextcolor), nudge_x = -.2, nudge_y = .15) + 
-  geom_tiplab(align=TRUE, linetype='dashed', linesize=.3) + 
+  geom_tiplab(align=FALSE, linetype='dashed', linesize=.3) + 
   theme(axis.text.x = element_text(size=12)) + xlab("mya")
-                    
-ggsave("mrca_tree.svg", width = 10, height = 5)
-ggsave("mrca_tree.png", width = 10, height = 5)
+#dev.off()                    
+ggsave("mrca_tree.svg", width = 6, height = 6)
+ggsave("mrca_tree.png", width = 6, height = 6)
